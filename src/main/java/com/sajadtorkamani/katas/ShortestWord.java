8a1771f7d455b680716157f6c7c5bfab62e73da8
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-09-11 18:55:41 +0100

    Solve kata

---
+++ src/main/java/com/sajadtorkamani/katas/ShortestWord.java
@@ -0,0 +1,14 @@
+package com.sajadtorkamani.katas;
+
+import java.util.Arrays;
+
+class ShortestWord {
+    static int findShort(String str) {
+        String[] words = str.split(" ");
+
+        return Arrays.stream(words)
+            .mapToInt(String::length)
+            .min()
+            .getAsInt();
+    }
+}
--- src/test/java/com/sajadtorkamani/katas/ExesAndOhsTest.java
+++ src/test/java/com/sajadtorkamani/katas/ExesAndOhsTest.java
@@ -2,7 +2,8 @@ package com.sajadtorkamani.katas;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 // https://www.codewars.com/kata/exes-and-ohs/train/java
 class ExesAndOhsTest {
