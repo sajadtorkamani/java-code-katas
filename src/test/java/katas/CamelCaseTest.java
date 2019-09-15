5fd7dc4ed54439370cdac33b144440c074b3c93a
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-09-15 10:34:13 +0100

    Refactor: tidy up tests

--- src/test/java/katas/CamelCaseTest.java
+++ src/test/java/katas/CamelCaseTest.java
@@ -7,38 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CamelCaseTest {
     @Test
-    public void testCamelBasic() {
-        assertEquals("TeSt",
-                CamelCase.cAmEl("test"));
-    }
-
-    @Test
-    public void testCamelEmpty() {
-        assertEquals("",
-                CamelCase.cAmEl(""));
-    }
-
-    @Test
-    public void testCamelAllCaps() {
-        assertEquals("TeSt",
-                CamelCase.cAmEl("TEST"));
-    }
-
-    @Test
-    public void testCamelStartWithCapital() {
-        assertEquals("TeSt",
-                CamelCase.cAmEl("Test"));
-    }
-
-    @Test
-    public void testCamelStartWithInvertedCamel() {
-        assertEquals("TeSt",
-                CamelCase.cAmEl("tEsT"));
-    }
-
-    @Test
-    public void testCamelOtherWord() {
-        assertEquals("IdOnTeVeN",
-                CamelCase.cAmEl("IDOntEvEN"));
+    public void cAmEl_returnsCorrectResult() {
+        assertEquals("TeSt", CamelCase.cAmEl("test"));
+        assertEquals("", CamelCase.cAmEl(""));
+        assertEquals("TeSt", CamelCase.cAmEl("TEST"));
+        assertEquals("TeSt", CamelCase.cAmEl("Test"));
+        assertEquals("TeSt", CamelCase.cAmEl("tEsT"));
+        assertEquals("IdOnTeVeN", CamelCase.cAmEl("IDOntEvEN"));
     }
 }
