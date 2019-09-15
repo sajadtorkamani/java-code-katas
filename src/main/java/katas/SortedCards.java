30d98f5189ae3729dd7abdef914b4f8eec0f505b
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-09-15 17:16:17 +0100

    Refactor: tidy up code

--- src/main/java/katas/SortedCards.java
+++ src/main/java/katas/SortedCards.java
@@ -5,21 +5,11 @@ import java.util.Comparator;
 
 class SortedCards {
     String[] sortCards(String[] cards) {
-        Arrays.sort(cards, new CardComparator());
+        var cardRanks = "A23456789TJQK";
+
+        Arrays.sort(cards, Comparator.comparingInt(cardRanks::indexOf));
 
         return cards;
     }
 }
 
-class CardComparator implements Comparator<String> {
-    @Override
-    public int compare(String card1, String card2) {
-        var cardRanks = "A23456789TJQK";
-
-        if (card1.equals(card2)) {
-            return 0;
-        }
-
-        return cardRanks.indexOf(card1) < cardRanks.indexOf(card2) ? -1 : 1;
-    }
-}
